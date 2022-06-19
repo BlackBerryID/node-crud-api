93cbf336701880aaecba6e059c3898ba6b66b1b4
BlackBerryID <iddf1994one@gmail.com>
2022-06-19 13:07:29 +0300

    feat: add 404 route

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,11 @@ const server = http.createServer((req, res) => {
   if (req.url === '/users') {
     getUsers(req, res);
   } else {
-    console.log('default');
+    res.writeHead(404, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
   }
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
