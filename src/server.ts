83f50adf13ecfedf2e6fbac70cc16940315b3e91
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-02-12 23:14:30 +0300

    feat: add scripts in package.json

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,7 @@ export const bootstrapServer = () => {
   const server = http.createServer();
   server.listen(process.env.PORT);
 
+  console.log(`Server is running on port ${process.env.PORT}`);
+
   return server;
 };
