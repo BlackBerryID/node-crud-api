a780c0d2fff93650dd66c4b63e76e829ecc5ad05
Dmitry Khomichenko <dmitrii.khomichenko@pobeda.aero>
2024-02-13 00:57:13 +0300

    fix: handle case when 3000 port is busy and user want to run tests

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,13 @@ export const bootstrapServer = (plusToPORT?: number) => {
     port += plusToPORT;
   }
 
+  server.once('error', function (err: any) {
+    if (err.code === 'EADDRINUSE') {
+      // port is currently in use
+      port = 5000;
+    }
+  });
+
   server.listen(port);
 
   console.log(`Server is running on port ${port}`);
